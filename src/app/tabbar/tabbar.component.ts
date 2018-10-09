6f52d20492c8f9595d88dc0485489155333f92ea
Farzad <sffatemi@gmail.com>
2018-10-10 09:06:07 +1300

    First Commit

---
+++ src/app/tabbar/tabbar.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-tabbar',
+  templateUrl: './tabbar.component.html',
+  styleUrls: ['./tabbar.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class TabbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
