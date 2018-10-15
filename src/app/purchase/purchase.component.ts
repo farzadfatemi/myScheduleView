b7fcef713d2aa4b9c973d80709f939be6fd0ac64
Farzad <sffatemi@gmail.com>
2018-10-16 11:29:10 +1300

    New Changes UI

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -1,15 +1,34 @@
 import {Component, OnInit} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-purchase',
   templateUrl: './purchase.component.html',
-  styleUrls: ['./purchase.component.css']
+  styleUrls: ['./purchase.component.css','./purchase.component.scss']
 })
 export class PurchaseComponent implements OnInit {
   title = 'myScheduleView';
   constructor() { }
 
   ngOnInit() {
+    // $(document).ready(function(){
+    //   $("button").click(function(){
+    //     var div = $("div");
+    //     div.animate({left: '100px'}, "slow");
+    //     div.animate({fontSize: '5em'}, "slow");
+    //   });
+    // });
+    $(document).ready(function(){
+      $("#bunnings").click(function(){
+        $("#bunnings").animate( { "opacity": "hide", top:"100"} , 500 );
+        $("#bunnings-in-list").animate( { "opacity": "show", top:"100"} , 500 );
+      });
+      $("#bunnings-in-list").click(function(){
+        $("#bunnings").animate( { "opacity": "show", top:"100"} , 500 );
+        $("#bunnings-in-list").animate( { "opacity": "hide", top:"100"} , 500 );
+      });
+    });
+
   }
 
 }
