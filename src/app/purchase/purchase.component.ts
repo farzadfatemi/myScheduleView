4a2829b5747ad02cb6e8f36c1c692a041cd8e40b
Farzad <sffatemi@gmail.com>
2018-10-16 12:27:48 +1300

    Work on Css/Jquery Animation

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -19,13 +19,28 @@ export class PurchaseComponent implements OnInit {
     //   });
     // });
     $(document).ready(function(){
+
       $("#bunnings").click(function(){
-        $("#bunnings").animate( { "opacity": "hide", top:"100"} , 500 );
-        $("#bunnings-in-list").animate( { "opacity": "show", top:"100"} , 500 );
+        $('#bunnings').addClass('animated rubberBand faster').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+          $('#bunnings').removeClass('animated rubberBand faster');
+        }).animate( { "opacity": "hide", top:"100"} , 500 );
+        $('#bunnings-in-list').addClass('animated bounceIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+          $('#bunnings-in-list').removeClass('animated bounceIn');
+        }).animate( { "opacity": "show", top:"100"} , 500 );
+        // $('#bunnings').animate( { "opacity": "hide", top:"100"} , 500 );
+        // $("#bunnings-in-list").animate( { "opacity": "show", top:"100"} , 500 );
       });
       $("#bunnings-in-list").click(function(){
-        $("#bunnings").animate( { "opacity": "show", top:"100"} , 500 );
-        $("#bunnings-in-list").animate( { "opacity": "hide", top:"100"} , 500 );
+        // $('#bunnings').removeClass('animated rubberBand faster').addClass('animated bounceInRight').animate( { "opacity": "show", top:"100"} , 500 );
+        // $('#bunnings-in-list').removeClass('animated bounceIn').addClass('animated bounceOut faster').animate( { "opacity": "hide", top:"100"} , 500 );
+        $('#bunnings').addClass('animated bounceInRight').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+          $('#bunnings').removeClass('animated bounceInRight');
+        }).animate( { "opacity": "show", top:"100"} , 500 );
+        $('#bunnings-in-list').addClass('animated bounceOut faster').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+          $('#bunnings-in-list').removeClass('animated bounceOut faster');
+        }).animate( { "opacity": "hide", top:"100"} , 500 );
+       // $('#bunnings').animate( { "opacity": "show", top:"100"} , 500 );
+       //  $('#bunnings-in-list').animate( { "opacity": "hide", top:"100"} , 500 );
       });
     });
 
