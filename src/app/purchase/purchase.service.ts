628c0e427e3d06ffc0267af6aad03277f31ba587
Farzad <sffatemi@gmail.com>
2018-10-24 15:14:57 +1300

    Working on server interaction

--- src/app/purchase/purchase.service.ts
+++ src/app/purchase/purchase.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Purchase} from '../domain/purchase';
 
+const URL = 'http://localhost:8080/';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,13 +11,13 @@ export class PurchaseService {
   constructor(private http: HttpClient) { }
 
   getAllPurchases() {
-    return this.http.get<any>('http://localhost:8080/allPurchases')
+    return this.http.get<any>(URL+'allPurchases')
       .toPromise()
       .then(res => <Purchase[]>res)
       .then(data => { return data; });
   }
-  addPurchases(id) {
-    return this.http.get<any>('http://localhost:8080/allPurchases')
+  addPurchases(file) {
+    return this.http.post((URL+'addPurchase'),file)
       .toPromise()
       .then(res => <Purchase[]>res)
       .then(data => { return data; });
